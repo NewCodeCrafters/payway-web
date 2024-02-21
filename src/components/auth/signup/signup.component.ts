94af1abbdf298476c6c5fc43e89920b47fce027f
gideon-del <gideonchidi471@gmail.com>
2024-02-22 00:03:42 +0100

    Signup component done

--- src/components/auth/signup/signup.component.ts
+++ src/components/auth/signup/signup.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
-import { SignUp } from '../components/auth/signup/signup.component';
+import { FormsModule } from '@angular/forms';
 @Component({
-  selector: 'app-root',
   standalone: true,
+  selector: 'sign-up',
+  templateUrl: './signup.component.html',
   imports: [
-    RouterOutlet,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
-    SignUp,
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'payway-web';
-}
+export class SignUp {}
